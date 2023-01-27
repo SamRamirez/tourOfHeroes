78d897fedcbdb595dfec8f8dca1254b7744d99f1
Samuel <sramirez@oxyl.fr>
2023-01-27 12:31:19 +0100

    separation des details du hero dan sle nouveau composant detail-hero

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+
+  @Input() hero?: Hero;
+
+  // selectedHero?: Hero;
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
+
+}
