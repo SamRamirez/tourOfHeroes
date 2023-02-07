358ee03392d263b2626e66a84f78cb0df58cc188
Samuel <sramirez@oxyl.fr>
2023-02-07 11:22:08 +0100

    utilisation d'un service pour récupérer le mock HEROES plutot que d'utiliser ce mock directement dans le composant

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  //heroes = HEROES;
+  heroes: Hero[] = [];
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
